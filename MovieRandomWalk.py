9436330c0a5321fc622d129e4d971d62bf78f883
G0D0T <42006598+G0D0T@users.noreply.github.com>
2019-09-17 19:53:09 +0200

    Aggiunti commenti e ultimi fix

--- MovieRandomWalk.py
+++ MovieRandomWalk.py
@@ -1,8 +1,10 @@
 import networkx as nx
 import numpy as np
 import pandas as pd
-import secrets as sc
+import secrets as sc #per la funzione random
 
+#funzione per estrarre il prossimo nodo in modo casuale, con la probabilità condizionata dal rating
+#la variabile t serve a limitare il sottoinsieme di nodi da estrarre: più il valore è alto e più nodi saranno filtrati
 def extract_neigh(node, t = 0.75):
 	neig = sorted(node.items(), key=lambda edge: edge[1]['rating'])
 	threshold = int(len(neig)*t)
@@ -10,31 +12,38 @@ def extract_neigh(node, t = 0.75):
 	next = sc.choice(neig)
 	return next[0]
 
+#funzione finale per la stampa a schermo di quanto trovato
 def consigli(reccomend, filt):
 	print('----------------------------------')
 	print('In base alla sua watchlist, le suggeriamo i seguenti film: ')
 	for f in reccomend[filt:]:
 		print(f)
 
+#set dei tipi delle colonne del dataset per una lettura più agevole
 typesMovies = {'movieId': np.dtype(int),'title': np.dtype(str),'genres' : np.dtype(str)}
 typesRatings = {'userId': np.dtype(int),'movieId': np.dtype(int),'rating': np.dtype(float),'timestamp' : np.dtype(int)}
 
+#import dei dataset originali
 movies = pd.read_csv('movies.csv', dtype=typesMovies, low_memory = False)
 ratings = pd.read_csv('ratings.csv', dtype=typesRatings, low_memory = False)
 
+#unione dei due dataset
 movie_ratings = pd.merge(movies, ratings)
 
+#drop delle colonne inutili
 movie_ratings.drop("timestamp", inplace = True, axis = 1)
 movie_ratings.drop("movieId", inplace = True, axis = 1)
 movie_ratings.drop("genres", inplace = True, axis = 1)
 
 #print(movie_ratings.sample(10))
 
+#generazione del grafo
 G = nx.from_pandas_edgelist(movie_ratings, 'userId', 'title', ['rating'])
 movie_ratings.to_csv('import.csv', index=False)
 print(nx.info(G))
 #nx.write_weighted_edgelist(G, 'movie.weighted.edgelist')
 
+#aggiungo un nuovo utente con relativi film visti
 newUser = 777
 G.add_edge(newUser, 'The Machinist (2004)', rating=5.0)
 G.add_edge(newUser, 'Harry Potter and the Prisoner of Azkaban (2004)', rating=4.0)
@@ -42,14 +51,12 @@ G.add_edge(newUser, 'Toy Story (1995)', rating=5.0)
 G.add_edge(newUser, 'Pulp Fiction (1994)', rating=5.0)
 G.add_edge(newUser, 'The Mask', rating=4.0)
 
-#print(G[184]['Flint (2017)']['rating']) #test
 #print([n for n in G.neighbors(2)]) #vedo tutti i film visti da un certo utente
 #print(G[2]) #vedo tutti i film visti da un certo utente con voto assegnato
 #print([n for n in G.neighbors('Whiplash (2014)')]) #vedo tutti gli utenti che hanno visto un certo film
 
-numSteps = 10
-#step = 0
-#flag = False #True per gli utenti, viceversa per i film
+#parametri globali
+numSteps = 10 #lunghezza prevista della random walk
 walk = []
 visitedFilm = []
 visitedUser = []
@@ -58,27 +65,32 @@ giavisti = ([n for n in G.neighbors(start)])#.items()
 
 def main():	
 	step = 0
-	flag = False
+	flag = False #True per gli utenti, viceversa per i film
 	walk.append(start)
 	visitedUser.append(start)
 	for f in giavisti:
-		visitedFilm.append(f)
+		visitedFilm.append(f) #salvo i film già visionati per evitare di consigliarli di nuovo
 	nodo = start
 	#prev = start
-	primoF = True
-	endd = 0
+	primoF = True #flag che indica se ci troviamo ad aggiungere il primo film
+	endd = 0 #variabile che evita loop infiniti (possibile in un certo caso)
 
 	while (step < numSteps) and (endd < 30):
-		if endd > 15:
-			nodo = visitedFilm[-2]
+		if endd > 15: #verifico di non trovarmi in un "vicolo cieco" e, in caso, ritorno indietro
+			if flag:				
+				nodo = visitedUser[-2]
+				flag = not flag
+			else:
+				nodo = visitedFilm[-2]
+				flag = not flag
 		endd += 1
 		nodo = extract_neigh(G[nodo])
-		#print(nodo)
+		print(nodo)
 		#print(prev)
 		if flag:
 			if nodo not in visitedUser:
 				visitedUser.append(nodo)			
-			else:
+			else:			#nel caso il nodo sia già stato visitato, passo alla prossima iterazione
 				nodo = prev
 				continue
 		else:
@@ -93,7 +105,7 @@ def main():
 				nodo = prev
 				continue
 
-		walk.append(nodo)
+		walk.append(nodo) #il nodo trovato in questa iterazione è nuovo, quindi devo aggiornare i seguenti parametri
 		prev = nodo
 		flag = not flag
 		step += 1
@@ -103,7 +115,7 @@ def main():
 main()
 
 """
-film visti per esempio
+funzioni utils
 
 for x in G.nodes():
       print ("Node:", x, "has total #degree:",G.degree(x), " , In_degree: ", G.out_degree(x)," and out_degree: ", G.in_degree(x))   
